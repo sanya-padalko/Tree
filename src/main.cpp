049e1b8d2715e66925a7a91c6b28af5780d994fd
sanya-padalko <apadalko90@gmail.com>
2025-11-11 20:37:25 +0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,34 +5,26 @@ int main(int argc, char* argv[]) {
     MakeDir();
     OpenHtml();
 
+    const char* base_file = "../base.txt";
+
     Tree_t* tree = TreeCtor();
 
-    tree->root->message = "animal";
-    Node_t* node1 = NodeCtor("cat");
-    Node_t* node2 = NodeCtor("have maces on its hands");
-    Node_t* node3 = NodeCtor("Poltorashka");
-    Node_t* node4 = NodeCtor("Skubi-du");
-    Node_t* node5 = NodeCtor("Mega Knight");
-    Node_t* node6 = NodeCtor("Hog rider");
+    ReadBase(tree, base_file);
 
-    tree->root->left = node1;
-    tree->root->right = node2;
-    node1->left = node3;
-    node1->right = node4;
-    node2->left = node5;
-    node2->right = node6;
+    char* cur_pos = tree->buf;
+    tree->root = ParseBase(&cur_pos);
 
-    AddVertex(node6, "have splash damage", "Mag");
-    AddVertex(node4, "have evolution", "King hogs");
-    AddVertex(node4->right, "collected from bones", "Sceleton Dragons");
-
-    tree->nodes_cnt = 13;
+    htmldump(tree, NOTHING, "base after parse");
 
     htmldump(tree, NOTHING, "from main #1");
 
     Akinator(tree);
 
     htmldump(tree, NOTHING, "from main #2");
+
+    FILE* base = fopen(base_file, "w");
+    TextDump(tree->root, base);
+    fclose(base);
 
     TreeDtor(tree->root);
     free(tree);
