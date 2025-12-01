dbb9fe7892b3db838ab38692d0dc8d130dfff20d
sanya-padalko <apadalko90@gmail.com>
2025-12-01 15:23:45 +0300

    Небольшие изменения базы и основной программы

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,30 @@
 #include "tree.h"
+#include "animation.h"
 #include "folders.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+    WindowCreating();
     MakeDir();
 
     const char* base_file = "../base.txt";
 
     Tree_t* tree = TreeCtor();
-
     ReadBase(tree, base_file);
-
     char* cur_pos = tree->buf;
     tree->root = ParseBase(&cur_pos);
     tree->root->parent = tree->root;
-
     htmldump(tree, NOTHING, "base after parse");
 
     Akinator(tree);
-
     htmldump(tree, NOTHING, "before writing to base");
 
-    FILE* base = fopen(base_file, "w");
-    TextDump(tree->root, base);
-    fclose(base);
+    if (argc > 1 && strcmp(argv[0], "-bc")) {
+        FILE* base = fopen(base_file, "w");
+        TextDump(tree->root, base);
+        fclose(base);
+    }
 
     TreeDtor(tree->root);
     free(tree);
+
 }
